6bd9b9581b1c1bb66413b783a472ded56f339394
FrankPeake <frankpeake1996@gmail.com>
2020-12-07 21:28:47 -0500

    first commit

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+$(function() {
+            
+    $('.menubtn').hover(function(){
+        $(this).css('background-color','#2C1FA8');
+        }, function(){
+        $(this).css('background-color','#12096C');
+    });
+    $('.logo img').hover(function(){
+        $(this).css('background-color','#2C1FA8');
+        }, function(){
+        $(this).css('background-color','#12096C');
+    });
+});
